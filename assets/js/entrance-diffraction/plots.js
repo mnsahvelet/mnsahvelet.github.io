672b1224c28948a28a243cbdca6ddae7df93d2e5
MUHAMMED NEDIM SAHVELET <96226458+mnsahvelet@users.noreply.github.com>
2026-02-24 12:09:55 -0500

    Update plots.js

--- assets/js/entrance-diffraction/plots.js
+++ assets/js/entrance-diffraction/plots.js
@@ -11,24 +11,33 @@
     };
   }
 
-  function plotMap(divId, xvals, yvals, Kd, Lb) {
-    const data = [{
-      type: "heatmap",
-      x: xvals,
-      y: yvals,
-      z: Kd,
-      colorbar: { title: "K<sub>d</sub>" }
-    }];
+function plotKdMap(divId, xvals, yvals, Kd, B) {
+  // Plotly heatmap expects z as [row(y)][col(x)]
+  const data = [{
+    type: "heatmap",
+    x: xvals,
+    y: yvals,
+    z: Kd,
+    zmin: 0,
+    zmax: 1,
+    colorbar: { title: "K\u1D05" }
+  }];
 
-    const layout = {
-      margin: { l: 70, r: 20, t: 40, b: 55 },
-      xaxis: { title: "Distance behind structure, dist (m)" },
-      yaxis: { title: "Position along from left, loc (m)" },
-      shapes: [ makeBreakwaterOverlay(Lb) ]
-    };
+  const layout = {
+    margin: { l: 60, r: 20, t: 30, b: 55 },
+    title: { text: "Diffraction Coefficient K\u1D05" },
+    xaxis: { title: "Distance behind structure, dist (m)" },
+    yaxis: { title: "Position along from left, loc (m)" },
+    shapes: [{
+      type: "line",
+      x0: 0, x1: 0,
+      y0: 0, y1: B,
+      line: { color: "black", width: 4 }
+    }]
+  };
 
-    Plotly.newPlot(divId, data, layout, {responsive: true});
-  }
+  Plotly.newPlot(divId, data, layout, {responsive:true});
+}
 
   function plotContour(divId, xvals, yvals, Kd, Lb, levels) {
     const contour = {
