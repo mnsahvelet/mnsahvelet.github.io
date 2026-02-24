82d14d212af7aa0aab8c4178c6501442eccf48b3
MUHAMMED NEDIM SAHVELET <96226458+mnsahvelet@users.noreply.github.com>
2026-02-24 03:36:49 -0500

    Create plots.js

---
+++ assets/js/entrance-diffraction/plots.js
@@ -0,0 +1,78 @@
+(() => {
+  "use strict";
+
+  function makeBreakwaterOverlay(Lb) {
+    // breakwater along x=0 from y=0..Lb
+    return {
+      type: "line",
+      x0: 0, x1: 0,
+      y0: 0, y1: Lb,
+      line: { color: "black", width: 4 }
+    };
+  }
+
+  function plotMap(divId, xvals, yvals, Kd, Lb) {
+    const data = [{
+      type: "heatmap",
+      x: xvals,
+      y: yvals,
+      z: Kd,
+      colorbar: { title: "K<sub>d</sub>" }
+    }];
+
+    const layout = {
+      margin: { l: 70, r: 20, t: 40, b: 55 },
+      xaxis: { title: "Distance behind structure, dist (m)" },
+      yaxis: { title: "Position along from left, loc (m)" },
+      shapes: [ makeBreakwaterOverlay(Lb) ]
+    };
+
+    Plotly.newPlot(divId, data, layout, {responsive: true});
+  }
+
+  function plotContour(divId, xvals, yvals, Kd, Lb, levels) {
+    const contour = {
+      type: "contour",
+      x: xvals,
+      y: yvals,
+      z: Kd,
+      contours: levels ? { start: levels[0], end: levels[levels.length-1], size: (levels[1]-levels[0]) } : {},
+      colorbar: { title: "K<sub>d</sub>" },
+      line: { width: 1 }
+    };
+
+    const layout = {
+      margin: { l: 70, r: 20, t: 40, b: 55 },
+      xaxis: { title: "Distance behind structure, dist (m)" },
+      yaxis: { title: "Position along from left, loc (m)" },
+      shapes: [ makeBreakwaterOverlay(Lb) ]
+    };
+
+    Plotly.newPlot(divId, [contour], layout, {responsive: true});
+  }
+
+  function plotCenterline(divId, xvals, centerKd) {
+    const data = [{
+      type: "scatter",
+      mode: "lines",
+      x: xvals,
+      y: centerKd,
+      line: { width: 2 }
+    }];
+
+    const layout = {
+      margin: { l: 70, r: 20, t: 40, b: 55 },
+      xaxis: { title: "Distance behind structure, dist (m)" },
+      yaxis: { title: "K<sub>d</sub> at loc = 0" }
+    };
+
+    Plotly.newPlot(divId, data, layout, {responsive: true});
+  }
+
+  window.ED_PLOTS = {
+    plotMap,
+    plotContour,
+    plotCenterline
+  };
+
+})();
